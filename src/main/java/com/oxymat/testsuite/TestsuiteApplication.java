771cae3ecc483502680c27f89651184282cd4a73
Unknown <chuwyn@live.dk>
2018-05-04 14:45:16 +0200

    Made Spring Project
    Created the Spring Project with dependencies.

---
+++ src/main/java/com/oxymat/testsuite/TestsuiteApplication.java
@@ -0,0 +1,12 @@
+package com.oxymat.testsuite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsuiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestsuiteApplication.class, args);
+	}
+}
---
+++ src/test/java/com/oxymat/testsuite/TestsuiteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.oxymat.testsuite;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestsuiteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
